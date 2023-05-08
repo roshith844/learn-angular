a36ddf0c705ad62a6470c3de332aca660217b21f
ROSHITH P <roshith844@gmail.com>
2023-05-08 11:10:51 +0530

    content: pipes

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TemplateReferenceVariablesComponent } from './components/template-reference-variables/template-reference-variables.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' 
-},
-{path: 'home', component: HomeComponent},
-{path: 'temp', component: TemplateReferenceVariablesComponent}
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'temp', component: TemplateReferenceVariablesComponent },
+  { path: 'pipes', component: PipesComponent }
 ];
 
 @NgModule({
--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TemplateReferenceVariablesComponent } from './components/template-reference-variables/template-reference-variables.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TemplateReferenceVariablesComponent
+    TemplateReferenceVariablesComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
